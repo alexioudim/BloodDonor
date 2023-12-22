a99dafafd31869712106491715d5977655a95fe2
Tasos <a.kanellias03@gmail.com>
2023-12-22 13:38:45 +0200

    created BloodDonor project

---
+++ src/test/java/dit/hua/team50/BloodDonor/BloodDonorApplicationTests.java
@@ -0,0 +1,13 @@
+package dit.hua.team50.BloodDonor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodDonorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
