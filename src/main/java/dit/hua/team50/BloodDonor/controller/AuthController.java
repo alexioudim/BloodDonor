736edefcfc20faeb8b981e4e6050136500dcdfb6
Dimitrios Alexiou <it2021002@hua.gr>
2024-01-12 15:22:42 +0200

    Added Roles PostConstruct

--- src/main/java/dit/hua/team50/BloodDonor/controller/AuthController.java
+++ src/main/java/dit/hua/team50/BloodDonor/controller/AuthController.java
@@ -1,5 +1,9 @@
 package dit.hua.team50.BloodDonor.controller;
 
+import dit.hua.team50.BloodDonor.entity.Role;
+import dit.hua.team50.BloodDonor.repository.RoleRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,6 +11,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AuthController {
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @PostConstruct
+    public void setup() {
+        roleRepository.findByName("ROLE_ADMIN").orElseGet(() -> {
+            roleRepository.save(new Role("ROLE_ADMIN"));
+            return null;
+        });
+        roleRepository.findByName("ROLE_USER").orElseGet(() -> {
+            roleRepository.save(new Role("ROLE_USER"));
+            return null;
+        });
+
+        roleRepository.findByName("ROLE_SECRETARY").orElseGet(() -> {
+            roleRepository.save(new Role("ROLE_SECRETARY"));
+            return null;
+        });
+    }
     @GetMapping("/login")
     public String login() {
         return "login";
