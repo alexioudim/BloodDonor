57be1aacf37eb68927c7bc9f89406294e1afc530
Dimitrios Alexiou <it2021002@hua.gr>
2024-02-20 17:08:20 +0200

    Removed Thymeleaf and templates

--- src/main/java/dit/hua/team50/BloodDonor/entity/Application.java
+++ src/main/java/dit/hua/team50/BloodDonor/entity/Application.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
-import java.util.Date;
-
 @Entity
 @Table(name = "Applications")
 public class Application {
